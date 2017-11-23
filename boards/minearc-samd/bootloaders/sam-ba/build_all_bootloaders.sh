87d6ddd7af8d5be2fa32e41f832383345c368155
mitchellp <mitchell.pontague@gmail.com>
2017-11-23 15:19:17 +0800

    Update core and transfer to minearc naming

---
+++ boards/minearc-samd/bootloaders/sam-ba/build_all_bootloaders.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+
+BOARD_ID=pathfinder NAME=pathfinder mingw32-make clean all
+
+echo Done building bootloaders!
+
--- boards/mitchellp-samd-1.0.0/bootloaders/samd21/build_all_bootloaders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-BOARD_ID=mitchellp_samd21g NAME=mitchellp_samd21g make clean all
-
-BOARD_ID=mitchellp_samd21e NAME=mitchellp_samd21e make clean all
-
-echo Done building bootloaders!
-
